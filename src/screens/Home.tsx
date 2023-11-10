ea5b6358fdb437ac3f125cac01152ea4d8fd07d5
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-11-10 11:50:24 -0800

    added navigation

---
+++ src/screens/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <nav className="flex-row justify-between bg-gray-200  px-6">
+    <h5 className="text-lg font-bold ">XY-College</h5>
+    Courses
+    Subjects
+    Grades
+    Analytics
+  </nav>
+    
+ 
+  )
+}
+
+export default Home

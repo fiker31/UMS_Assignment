5b97b9d9fda23ea9c84f7cb06073e3c93966d510
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-11-10 11:19:07 -0800

    imported home

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 
-import './index.css'
+import Home from "./screens/Home";
 
 function App ()
 {
   return
   (
     <div>
-      <div className="">
-    
-      </div>
+     <Home/>
     </div>
   )
 }

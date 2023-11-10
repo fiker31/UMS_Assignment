d8512da20556d675e59c9efd43873f2687517775
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-11-10 11:05:54 -0800

    initialize project

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+
+function header()
+{
+    return 
+    (
+        <div className="flex-1">
+            
+        </div>
+    )
+}
+export default header;
---
+++ src/components/Home.tsx
@@ -0,0 +1,10 @@
+import header from "./Header";
+function home()
+{
+    return (
+        <div className="flex-1">
+            
+        </div>
+    )
+}
+export default home;
